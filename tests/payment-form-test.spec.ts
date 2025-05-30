07843dfd577fd7b7a2d4fd413ec3e78cab636d55
Dmytro Zubenko <fokist@outlook.com>
2025-05-30 22:56:17 +0300

    test data separation

--- tests/payment-form-test.spec.ts
+++ tests/payment-form-test.spec.ts
@@ -3,30 +3,25 @@ import {expect, test} from '@playwright/test';
 import { MenuPage } from '@pages/menu-page';
 import { PaymentModal } from '@modal/payment-detail-modal';
 import { SuccessfulModal } from '@modal/successful-modal';
+import { PaymentTestData as Data } from '@tests/data/payment-test-data';
 
 test.describe('Verify Payment Form Accepts Valid Inputs', () => {
-    const VALID_NAME = 'Andrii';
-    const VALID_EMAIL = 'andrii11@gmail.com';
-    const TESTING_DRINK_NAME = 'Flat White';
-    const SUCCESS_MESSAGE = 'Thanks for your purchase. Please check your email for payment.';
-    const EMPTY_CART_VALUES = 'Total: $0.00';
-
     test('should submit payment form with valid inputs', async ({ page }) => {
         const menuPage = new MenuPage(page);
         const paymentModal = new PaymentModal(page);
         const successModal = new SuccessfulModal(page);
 
         await menuPage.visit();
-        await menuPage.clickOnDrink(TESTING_DRINK_NAME);
+        await menuPage.clickOnDrink(Data.testingDrink);
         await menuPage.clickTotalButton();
-        await paymentModal.enterName(VALID_NAME);
-        await paymentModal.enterEmail(VALID_EMAIL);
+        await paymentModal.enterName(Data.validName);
+        await paymentModal.enterEmail(Data.validEmail);
         await paymentModal.clickSubmit();
 
         const successMessageText = await successModal.getMessageText();
-        expect(successMessageText.trim()).toBe(SUCCESS_MESSAGE);
+        expect(successMessageText.trim()).toBe(Data.successMessage);
 
         const totalText = await menuPage.getTotalButtonText();
-        expect(totalText.trim()).toBe(EMPTY_CART_VALUES);
+        expect(totalText.trim()).toBe(Data.emptyCartText);
     });
 });
