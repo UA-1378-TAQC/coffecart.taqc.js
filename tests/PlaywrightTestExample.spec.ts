b3937900495c3437519aec782bce8eccb7446e30
MarryMik <57491436+MarryMik@users.noreply.github.com>
2025-05-31 04:41:42 +0300

    logger was added

--- tests/PlaywrightTestExample.spec.ts
+++ tests/PlaywrightTestExample.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
+import log from '../utils/logger';
 
 test('Playwright website navigation test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
+  log.info('Info');
   await expect(page.locator('//*[@id=\"__docusaurus_skipToContent_fallback\"]/header/div/h1')).toContainText("Playwright enables reliable end-to-end testing for modern web apps");
 
   await expect(page.locator('nav')).toBeVisible();
